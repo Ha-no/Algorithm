7a4dce5653d277c111f3b3ad1f4df8b384c5b918
Ha-no <97273652+Ha-no@users.noreply.github.com>
2022-04-10 23:14:59 +0900

    Update A02.java

--- 0/A02.java
+++ 0/A02.java
@@ -5,7 +5,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class A2 {
+public class A02 {
     public static void main(String[] args) throws Exception{
 
     	BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
