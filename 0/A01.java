6201a23f455a049946ab6b666199ce12292efdee
Ha-no <97273652+Ha-no@users.noreply.github.com>
2022-04-10 23:14:50 +0900

    Update A01.java

--- 0/A01.java
+++ 0/A01.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class A1{
+public class A01{
     public static void main(String[] args) throws Exception{
 
     	// 입력
