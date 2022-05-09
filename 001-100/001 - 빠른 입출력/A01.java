9b02f57ed819e555e04fd30753aa70e3efdca352
Ha-no <97273652+Ha-no@users.noreply.github.com>
2022-05-09 12:21:00 +0900

    Update and rename 0/A01.java to 001-100/001 - 빠른 입출력/A01.java

--- 001-100/001 - 빠른 입출력/A01.java
+++ 001-100/001 - 빠른 입출력/A01.java
@@ -1,12 +1,4 @@
 
-
-
-
-/*	
-	¹®Á¦ : https://www.acmicpc.net/problem/15552
-	Ç®ÀÌ ³ë¼Ç : https://www.notion.so/5bc82b0dba0949c6a4db69371b1ad65b
-*/
-
 package Algorithm;
 
 import java.io.BufferedReader;
@@ -18,31 +10,31 @@ import java.util.StringTokenizer;
 public class A01{
     public static void main(String[] args) throws Exception{
 
-    	// ÀÔ·Â
+    	// ì…ë ¥
     	BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-    	// Ãâ·Â
+    	// ì¶œë ¥
     	BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
     	
-    	// ¹®ÀÚ¿­À» ³ª´©°Ô µÇ´Âµ¥ Default°ªÀº °ø¹é/ÅÇ/ÁÙ¹Ù²Ş µî ±âº» ±¸ºĞÀÚ°¡ Àû¿ëµÇ¾î ³ª´©°Ô µÊ
+    	// ë¬¸ìì—´ì„ ë‚˜ëˆ„ê²Œ ë˜ëŠ”ë° Defaultê°’ì€ ê³µë°±/íƒ­/ì¤„ë°”ê¿ˆ ë“± ê¸°ë³¸ êµ¬ë¶„ìê°€ ì ìš©ë˜ì–´ ë‚˜ëˆ„ê²Œ ë¨
     	StringTokenizer st = new StringTokenizer( br.readLine() );
-    	// ¹®ÀÚ¿­À» ³ª´­ ¶§ »ç¿ëÀÚ°¡ ÁöÁ¤ÇÑ ¹®ÀÚ/¹®ÀÚ¿­·Î ³ª´©°Ô µÊ
+    	// ë¬¸ìì—´ì„ ë‚˜ëˆŒ ë•Œ ì‚¬ìš©ìê°€ ì§€ì •í•œ ë¬¸ì/ë¬¸ìì—´ë¡œ ë‚˜ëˆ„ê²Œ ë¨
     	StringTokenizer st1 = new StringTokenizer( br.readLine(), "T" );
-    	// ¹®ÀÚ¿­À» ³ª´­ ¶§ »ç¿ëÀÚ°¡ ÁöÁ¤ÇÑ ¹®ÀÚ/¹®ÀÚ¿­À» ³ª´©°í ±âº» ±¸ºĞÀÚµéµµ ³ª´­Áö¸¦ true/false·Î Àû¿ëÇÏ¿© ³ª´©°Ô µÊ
-        // ex) aTbTcTÀÔ·Â½Ã >> TrueÀÏ¶§ : a T b T c  / FalseÀÏ¶§ : a b c
+    	// ë¬¸ìì—´ì„ ë‚˜ëˆŒ ë•Œ ì‚¬ìš©ìê°€ ì§€ì •í•œ ë¬¸ì/ë¬¸ìì—´ì„ ë‚˜ëˆ„ê³  ê¸°ë³¸ êµ¬ë¶„ìë“¤ë„ ë‚˜ëˆŒì§€ë¥¼ true/falseë¡œ ì ìš©í•˜ì—¬ ë‚˜ëˆ„ê²Œ ë¨
+        // ex) aTbTcTì…ë ¥ì‹œ >> Trueì¼ë•Œ : a T b T c  / Falseì¼ë•Œ : a b c
     	StringTokenizer st2 = new StringTokenizer( br.readLine(), "T", true );
     	
-    	// ¹®ÀÚ¿­·Î ÀĞÀº ³»¿ëÀ» intÇüÀ¸·Î Çüº¯È¯
+    	// ë¬¸ìì—´ë¡œ ì½ì€ ë‚´ìš©ì„ intí˜•ìœ¼ë¡œ í˜•ë³€í™˜
     	int input1 = Integer.parseInt( st.nextToken() );
     	int input2 = 0, input3 = 0;
     	
-    	// ¸Ş¸ğ¸® »ç¿ëÀ» ÁÙÀÌ±â À§ÇÑ StringBuffer
+    	// ë©”ëª¨ë¦¬ ì‚¬ìš©ì„ ì¤„ì´ê¸° ìœ„í•œ StringBuffer
     	StringBuffer sb1 = new StringBuffer();
     	
     	for( int i=0; i<input1; i++ ) {
     		
-    		// ÁÙ¹Ù²ã¼­ ÀÔ·ÂÇÒ ¶§ ¸¶´Ù Àç ¼±¾ğ
+    		// ì¤„ë°”ê¿”ì„œ ì…ë ¥í•  ë•Œ ë§ˆë‹¤ ì¬ ì„ ì–¸
     		st = new StringTokenizer( br.readLine() );
-            // StringTokenizer·Î ³ª´« ÅäÅ«µéÀ» Â÷·Ê´ë·Î »ç¿ë
+            // StringTokenizerë¡œ ë‚˜ëˆˆ í† í°ë“¤ì„ ì°¨ë¡€ëŒ€ë¡œ ì‚¬ìš©
     		input2 = Integer.parseInt( st.nextToken() );
     		input3 = Integer.parseInt( st.nextToken() );
     		sb1.append( input2 + input3 ).append( "\n" );
