acd83c5d8953c4c41691c97f8b699ee92ecda605
Ha-no <97273652+Ha-no@users.noreply.github.com>
2022-05-24 11:58:21 +0900

    Add files via upload

---
+++ 001-100/017 - 팩토리얼 0의 개수 ( 1676 )/A17.java
@@ -0,0 +1,35 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class A17 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringBuilder output = new StringBuilder();
+        
+        int n = Integer.parseInt( in.readLine() );
+        int count = 0;
+        
+        while( n >= 5 ) {
+        	
+        	count += ( n / 5 );
+        	n /= 5;
+        }
+
+        output.append( count );
+        
+        out.write( output.toString() );
+        
+		out.flush();
+		out.close();
+		in.close();
+	}
+	
+}
