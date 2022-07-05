b2bf3fb46f01118191d4056d00f328ec6c22f867
Ha-no <97273652+Ha-no@users.noreply.github.com>
2022-07-05 21:34:04 +0900

    Add files via upload

---
+++ 001-100/006 - 골드바흐의 추측 ( 9020 )/A06.java
@@ -0,0 +1,63 @@
+
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class A06{
+	
+	public static boolean[] array = new boolean[20001];
+	
+    public static void main(String[] args) throws Exception{
+
+    	BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+    	BufferedWriter out = new BufferedWriter( new OutputStreamWriter( System.out ) );
+
+    	StringBuffer sb = new StringBuffer();
+    	
+    	prime();
+    	
+    	int t = Integer.parseInt( in.readLine() );
+    	
+    	while( t-- > 0 ) {
+    		
+    		int n = Integer.parseInt( in.readLine() );
+    		int first = n/2, second = n/2;
+    		
+    		while( true ) {
+    			if( !array[first] && !array[second] ) {
+        		
+        			sb.append( first ).append( " " ).append( second ).append( "\n" );
+        			break;
+        		}
+    			first--;
+    			second++;
+    		}
+    		
+    	}
+    	
+    	out.write( sb.toString() );
+    	
+    	out.flush();
+    	out.close();
+    	in.close();
+    }
+    
+    public static void prime() {
+    	
+    	array[0] = array[1] = true;
+    	
+    	for( int a=2; a<=Math.sqrt( array.length ); a++ ) {
+    		
+    		if( !array[a] ) {
+    			
+    			for( int b=a*a; b<array.length; b+=a ) {
+    				
+    				array[b] = true;
+    			}
+    		}
+    	}
+    }
+}
